3810924aeff5506edca151554f9749e939250703
Jens Vercammen <verc.jens@gmail.com>
2024-01-25 16:34:35 +0100

    add default locale again

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,19 +6,19 @@ const PUBLIC_FILE = /\.(.*)$/
 // https://nextjs.org/docs/advanced-features/i18n-routing#prefixing-the-default-locale
 
 export async function middleware(req: NextRequest) {
-  // if (
-  //   req.nextUrl.pathname.startsWith('/_next') ||
-  //   req.nextUrl.pathname.includes('/api/') ||
-  //   PUBLIC_FILE.test(req.nextUrl.pathname)
-  // ) {
-  //   return
-  // }
-  //
-  // if (req.nextUrl.locale === 'default') {
-  //   const locale = req.cookies.get('NEXT_LOCALE')?.value || 'nl'
-  //
-  //   return NextResponse.redirect(
-  //     new URL(`/${locale}${req.nextUrl.pathname}${req.nextUrl.search}`, req.url)
-  //   )
-  // }
+  if (
+    req.nextUrl.pathname.startsWith('/_next') ||
+    req.nextUrl.pathname.includes('/api/') ||
+    PUBLIC_FILE.test(req.nextUrl.pathname)
+  ) {
+    return
+  }
+
+  if (req.nextUrl.locale === 'default') {
+    const locale = req.cookies.get('NEXT_LOCALE')?.value || 'nl'
+
+    return NextResponse.redirect(
+      new URL(`/${locale}${req.nextUrl.pathname}${req.nextUrl.search}`, req.url)
+    )
+  }
 }
